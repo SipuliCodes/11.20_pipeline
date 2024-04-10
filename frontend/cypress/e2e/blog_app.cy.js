77cf5bbcfc66a2cab6a3e0746b4c6d9baa330f24
SipuliCodes <vepsalainensisu@gmail.com>
2024-04-10 20:58:18 +0300

    added login to cypress

--- frontend/cypress/e2e/blog_app.cy.js
+++ frontend/cypress/e2e/blog_app.cy.js
@@ -91,7 +91,11 @@ describe('Blog app', function () {
 
     describe('When multible blogs are created', function () {
       beforeEach(function () {
-        cy.contains('Log in to aplication')
+        cy.contains('login').click()
+        cy.get('#username').type('käyttis')
+        cy.get('#password').type('Tuttiritari')
+        cy.get('#login-button').click()
+        cy.contains('Nakki Prinssi logged in')
         cy.contains('create new blog').click()
         cy.get('[placeholder="title"]').type('The title with the second most likes')
         cy.get('[placeholder="author"]').type('Mr Blog')
