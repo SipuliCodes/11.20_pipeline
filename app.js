74d7734ecaac934a079a6dc4b60a8f6228f91938
SipuliCodes <vepsalainensisu@gmail.com>
2024-04-10 23:07:25 +0300

    dsadsa

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ mongoose.set('strictQuery', false)
 
 logger.info('connecting to', config.MONGODB_URI)
 
+console.log(config.MONGODB_URI)
+
 mongoose.connect(config.MONGODB_URI)
     .then(() => {
         logger.info('connected to MongoDB')
